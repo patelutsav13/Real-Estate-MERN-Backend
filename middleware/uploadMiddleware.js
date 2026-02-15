c5428a1fd8924a8e77aaae9b45af39728e3da0ed
patel_utsav_1318 <patelutsav312@gmail.com>
2026-02-15 17:47:47 +0530

    Update uploadMiddleware.js

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -2,17 +2,8 @@ import multer from 'multer';
 import path from 'path';
 
 // Set storage engine
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + '-' + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
+// Set storage engine
+const storage = multer.memoryStorage();
 
 // Init upload
 const upload = multer({
@@ -41,6 +32,7 @@ function checkFileType(file, cb) {
 }
 
 export default upload;
+
 
 
 
